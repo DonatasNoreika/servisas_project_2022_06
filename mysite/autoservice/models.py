0805e425939d2a5859ed2f713ff5950972a114ce
Donatas Noreika <donoras@gmail.com>
2022-06-23 08:56:39 +0300

    Added models

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
+
 # Create your models here.
+
+class VehicleModel(models.Model):
+    year = models.IntegerField(verbose_name='Year')
+    make = models.CharField(verbose_name='Make', max_length=200)
+    model = models.CharField(verbose_name='Model', max_length=200)
+    engine = models.CharField(verbose_name='Engine', max_length=200)
+
+    def __str__(self):
+        return f"{self.year}, {self.make} {self.model}, {self.engine}"
+
+
+class Vehicle(models.Model):
+    owner_name = models.CharField(verbose_name='Owner', max_length=200)
+    vehicle_model = models.ForeignKey(to='VehicleModel', verbose_name="Vehicle Model", on_delete=models.SET_NULL,
+                                      null=True)
+    license_plate = models.CharField(verbose_name='License Plate', max_length=200)
+    vin_code = models.CharField(verbose_name='VIN code', max_length=20)
+
+    def __str__(self):
+        return f"{self.owner_name}: {self.vehicle_model}, {self.license_plate}, {self.vin_code}"
+
+
+class Service(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=200)
+    price = models.FloatField(verbose_name="Price")
+
+
+class Order(models.Model):
+    vehicle = models.ForeignKey(to='Vehicle', verbose_name="Vehicle", on_delete=models.SET_NULL, null=True)
+    due_date = models.DateTimeField(verbose_name="Due Date", null=True, blank=True)
+    tsum = models.FloatField(verbose_name="TOTAL")
+
+    ORDER_STATUS = (
+        ('a', 'Accepted'),
+        ('i', 'In Progress'),
+        ('d', 'Done'),
+        ('c', 'Cancelled'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=ORDER_STATUS,
+        blank=True,
+        default='a',
+        help_text='Status',
+    )
+
+class OrderLine(models.Model):
+    order = models.ForeignKey(to="Order", on_delete=models.CASCADE, null=True)
+    service = models.ForeignKey(to="Service", on_delete=models.SET_NULL, null=True)
+    qty = models.IntegerField(verbose_name="Quantity")
