2f53fbd8de8f38b302580a9bc720d6f9da06f060
Donatas Noreika <donoras@gmail.com>
2022-06-28 08:31:33 +0300

    4 done

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import (Service,
+                     Order,
+                     Vehicle)
+
 
 # Create your views here.
+def index(request):
+    services = Service.objects.count()
+    orders_done = Order.objects.filter(status__exact='d').count()
+    vehicles = Vehicle.objects.count()
+
+    my_context = {
+        "my_services": services,
+        "my_orders_done": orders_done,
+        "my_vehicles": vehicles,
+    }
+
+    return render(request, 'index.html', context=my_context)
