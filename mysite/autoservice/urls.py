fb235f567e4603b017151978c3a69b4ee34452b0
Donatas Noreika <donoras@gmail.com>
2022-06-29 08:21:00 +0300

    5 vehicle list and detail done

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("vehicles", views.vehicles, name="vehicles"),
+    path("vehicle/<int:vehicle_id>", views.vehicle, name="vehicle"),
 ]
--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import (Service,
                      Order,
                      Vehicle)
@@ -17,3 +17,13 @@ def index(request):
     }
 
     return render(request, 'index.html', context=my_context)
+
+
+def vehicles(request):
+    vehicles = Vehicle.objects.all()
+    return render(request, 'vehicles.html', context={"vehicles": vehicles})
+
+
+def vehicle(request, vehicle_id):
+    vehicle = get_object_or_404(Vehicle, pk=vehicle_id)
+    return render(request, 'vehicle.html', context={"vehicle": vehicle})
